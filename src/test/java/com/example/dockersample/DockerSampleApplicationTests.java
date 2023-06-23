339ff315e7fbe1d7394f4b2032c4011411762411
Sodai-Komichi <83051052+Aso2101122@users.noreply.github.com>
2023-06-23 11:23:51 +0900

    初期コミット

---
+++ src/test/java/com/example/dockersample/DockerSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockersample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
